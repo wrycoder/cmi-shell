2f2a61181649c2a9be158e6c6dd60761cbefb43e
Michael Toulouse <wrycoder@ameritech.net>
2021-08-12 13:38:11 -0400

    Script was still rounding up minutes at .9

--- vt-timings.sh
+++ vt-timings.sh
@@ -11,11 +11,7 @@ echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.$3)/(10^$2)" | bc))
 pad()
 {
 if [ "$1" -lt "10" ]; then
-  if [ "$1" -lt "6" ]; then
-    printf %s0 "$1"
-  else
-    printf 0%s "$1"
-  fi
+  printf 0%s "$1"
 else
   if [ "$1" -eq "60" ]; then
     printf %s "00"
@@ -31,4 +27,4 @@ SOXI=~/sox-14.4.2/soxi
 minutes=$($SOXI -D ~/radio/voicetracks-2/$1*.wav |awk 'BEGIN { s = 0 }; { s = s + $1 }; END { print s / 60 }')
 seconds=$($SOXI -D ~/radio/voicetracks-2/$1*.wav |awk 'BEGIN { s = 0 }; { s = s + $1 }; END { print s % 60 }')
 
-echo "$(round $minutes 0 1):$(pad $(round $seconds 0 5))"
+echo "$(round $minutes 0 0):$(pad $(round $seconds 0 5))"
