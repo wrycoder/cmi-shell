161df3c6a94785ccc4628193099ae72b2b893e0e
Michael Toulouse <wrycoder@ameritech.net>
2021-08-12 06:23:38 -0400

    Upgrading timings script with human-readable display

--- vt-timings.sh
+++ vt-timings.sh
@@ -1,5 +1,30 @@
 #!/bin/zsh
+# the function "round()" was taken from
+# http://stempell.com/2009/08/rechnen-in-bash/
 
-export SOXI=~/sox-14.4.2/soxi
+# the round function:
+round()
+{
+echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc))
+};
 
-$SOXI -D ~/radio/voicetracks-2/$1*.wav |awk 'BEGIN { s = 0 }; { s = s + $1 }; END { print s / 60 " minutes and " s % 60 " seconds" }'
+pad()
+{
+if [ "$1" -lt "10" ]; then
+  if [ "$1" -lt "6" ]; then
+    printf %s0 "$1"
+  else
+    printf 0%s "$1"
+  fi
+else
+  printf %s "$1"
+fi
+};
+
+SOXI=~/sox-14.4.2/soxi
+
+#$SOXI -D ~/radio/voicetracks-2/$1*.wav |awk 'BEGIN { s = 0 }; { s = s + $1 }; END { print s / 60 " minutes and " s % 60 " seconds" }'
+minutes=$($SOXI -D ~/radio/voicetracks-2/$1*.wav |awk 'BEGIN { s = 0 }; { s = s + $1 }; END { print s / 60 }')
+seconds=$($SOXI -D ~/radio/voicetracks-2/$1*.wav |awk 'BEGIN { s = 0 }; { s = s + $1 }; END { print s % 60 }')
+
+echo "$(round $minutes 0):$(pad $(round $seconds 0))"
