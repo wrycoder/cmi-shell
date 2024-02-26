ca0bc44ef133a56c470e6c2f9d27baecb2911079
Michael Toulouse <wrycoder@ameritech.net>
2024-02-26 10:05:29 -0500

    Simplified display. Now just showing total seconds without formatting

--- time-remaining.sh
+++ time-remaining.sh
@@ -1,25 +1,16 @@
-#!/bin/zsh
+#!/bin/bash
 
-# Adapted from sox.c
-str_time()
-{
-  seconds=$1
-  minutes=`echo "$1 / 60" |bc`
-  sval=`echo "$seconds - ($minutes * 60)" |bc`
-  seconds=`echo $sval |sed -n 's/\([0-9]*\)[.][0-9]*/\1/p'`
-  if [[ $seconds -eq "" ]]
-  then
-    seconds="0"
-  fi
-  decimal=`echo $sval |sed -n 's/[0-9]*[.]\([0-9]*\)/\1/p'`
-  printf "%02d:%02d.%s\n" $minutes $seconds $decimal
-};
 
 SOX=~/sox-14.4.2/sox
 # Maximum time in an hour is 56 minutes
 MAX_DURATION=`echo "60 * 56" |bc`
 
-filled=`$SOX $1*$2*.wav -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
+`/bin/ls $1*$2*.wav >/dev/null 2>&1`
+if [ $? -eq 0 ]; then
+  filled=`$SOX $1*$2*.wav -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
+else
+  filled=0
+fi
 
 unfilled=`echo "$MAX_DURATION - ($3 + $filled)" |bc`
-str_time $unfilled
+echo $unfilled
