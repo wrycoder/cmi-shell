ff6d7b171122b810a5f2d17c3b992770c12f35a9
Michael Toulouse <wrycoder@ameritech.net>
2024-05-05 15:40:22 -0400

    Now handling five-digit base names

--- fetch-remote-file.bat
+++ fetch-remote-file.bat
@@ -1,16 +1,16 @@
 @echo off
 
 set target=C5455-01-03.wav
-echo %target%
+rem set target=C10574-19.wav
 set number=%target:C=%
-set number=%number:~0,4%
-
-echo %number%
+set number=%number:~0,5%
+set number=%number:\-=%
 set /a "lownum=number - (number %% 2000)"
 set /a "hinum=lownum + 1999"
 set sharename=CD%lownum%-%hinum%
 set /a "lownum=number - (number %% 100)"
 set /a "hinum=lownum + 99"
+echo \\fas-media\%sharename%\%lownum%-%hinum%
 pushd \\fas-media\%sharename%\%lownum%-%hinum%
 copy %target% c:\exports
 popd
