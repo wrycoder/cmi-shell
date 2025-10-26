8279e161bb4d59a868e5033fe5b8266934aec1d7
Michael Toulouse <wrycoder@ameritech.net>
2025-10-26 16:12:05 -0400

    Add a check for the argument passed

--- extract-wav-files.sh
+++ extract-wav-files.sh
@@ -5,6 +5,11 @@
 # schedule. Currently focuses on programs B, C, G, H, I, J, and K.
 #
 
+if [ "$#" -ne 1 ]; then
+  printf "Error: you must specify a filename for input\n"
+  exit 1
+fi
+
 date=`sed -nE 's/.*([0-9]{4}-[0-9]{2}-[0-9]{2}).*/\1/p' $1 | head -1`
 
 output="$date.csv"
